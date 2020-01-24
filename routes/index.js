4666c59ee1698b560907d1a65edf46606bc49633
Jenaro <jen.calvineo@gmail.com>
2020-01-24 09:32:40 -0300

    Added img upload and resize

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const storeController = require('../controllers/storeController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/', storeController.getStores);
-router.get('/stores', catchErrors(storeController.getStores));
+router.get('/', storeController.getStores)
+router.get('/stores', catchErrors(storeController.getStores))
 
-router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/add', storeController.addStore)
+router.post(
+  '/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+)
+router.post(
+  '/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+)
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 
-module.exports = router;
+module.exports = router
